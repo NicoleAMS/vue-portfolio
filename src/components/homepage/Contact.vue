624505ced011b62ffa46bb27b06e19a04305ff87
NicoleAMS <nicole.sentis@gmail.com>
2020-12-11 15:21:40 +0100

    adds contact section

--- src/components/homepage/Contact.vue
+++ src/components/homepage/Contact.vue
@@ -1,15 +1,173 @@
 <template>
   <div id="contact-container">
-    <h3>Contact Component</h3>
-    <hr>
+    <div class="contact-row">
+      <div class="contact-left">
+        <img src="../../assets/images/desk.jpg" alt="" />
+      </div>
+
+      <div class="contact-right">
+        <div class="contact-text">
+          <h2>Get in touch</h2>
+          <div class="line"></div>
+          <p>
+            If you like my portfolio and are interested to find out more about
+            me and what I can do for you, please drop me a line at:
+            <a href="mailto: nicole.sentis@gmail.com"
+              >nicole.sentis@gmail.com</a
+            >
+          </p>
+          <br />
+          <p>Or find me on social media:</p>
+          <div class="social-media">
+            <a href="https://www.linkedin.com/in/nicolesentis/"
+              ><font-awesome-icon
+                :icon="['fab', 'linkedin']"
+                :class="['fa-icon']"
+                :style="['color: #ffa46b']"
+            /></a>
+            <a href="https://github.com/NicoleAMS"
+              ><font-awesome-icon :icon="['fab', 'github']" :class="['fa-icon']"
+            /></a>
+            <a href="https://codepen.io/nicole-sen/"
+              ><font-awesome-icon
+                :icon="['fab', 'codepen']"
+                :class="['fa-icon']"
+            /></a>
+          </div>
+          <br />
+          <p>Location: Hoorn (NH), The Netherlands</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss">
+$orange: #ffa46b;
+$aqua: #3c95a8;
+$darkgrey: #1a1a1a;
+$lightgrey: #c4beb5;
+$midnight: #010a1c;
+$dark: #081626;
+$white: #f1f7f3;
 
+#contact-container {
+  height: 70vh;
+
+  @media screen and (max-width: 768px) {
+    height: auto;
+    padding: 0;
+  }
+}
+
+.contact-row {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column-reverse;
+  }
+}
+
+.contact-left {
+  width: 70%;
+  height: 100%;
+  position: relative;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    @media screen and (max-width: 768px) {
+      max-height: 700px;
+    }
+  }
+}
+
+.contact-right {
+  position: absolute;
+  top: 15%;
+  right: 0%;
+  height: 70%;
+  width: 50%;
+  background-color: $aqua;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .contact-text {
+    margin: 10%;
+    text-align: right;
+    position: relative;
+
+    @media screen and (max-width: 1200px) {
+      margin: 5%;
+    }
+  }
+
+  @media screen and (max-width: 1440px) {
+    height: 60%;
+    top: 20%;
+  }
+
+  @media screen and (max-width: 1200px) {
+    top: 10%;
+    height: 80%;
+    width: 65%;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    height: 500px;
+    position: relative;
+  }
+
+  .line {
+    width: 100%;
+    height: 10px;
+    background-color: $orange;
+    margin-bottom: 25px;
+  }
+
+  h2 {
+    font-family: "Titillium Web", sans-serif;
+    font-size: 70px;
+    text-transform: uppercase;
+    text-align: right;
+  }
+
+  p {
+    font-family: "Scope One", serif;
+    font-size: 18px;
+    width: 85%;
+    position: relative;
+    left: 15%;
+  }
+
+  a {
+    color: $orange;
+    text-decoration: none;
+    &:hover {
+      path {
+        color: $orange;
+      }
+    }
+  }
+
+  .fa-icon {
+    font-size: 24px;
+    margin-left: 12px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas, faLinkedin, faGithub, faCodepen);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
+
