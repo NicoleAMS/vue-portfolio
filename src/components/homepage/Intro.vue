d6b259e6c8d666d3c7a3a5cbf64afae323298020
NicoleAMS <nicole.sentis@gmail.com>
2020-12-11 16:54:20 +0100

    makes intro component responsive

--- src/components/homepage/Intro.vue
+++ src/components/homepage/Intro.vue
@@ -9,62 +9,75 @@
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss">
-  $orange: #ffa46b;
-  $aqua: #3c95a8;
-  $darkgrey: #1A1A1A;
-  $lightgrey: #C4BEB5;
-  $midnight: #010A1C;
+$orange: #ffa46b;
+$aqua: #3c95a8;
+$darkgrey: #1a1a1a;
+$lightgrey: #c4beb5;
+$midnight: #010a1c;
+$dark: #081626;
+$white: #f1f7f3;
 
-  #intro-container {
+#intro-container {
+  height: 100vh;
+  background-image: url("../../assets/images/background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  .intro-text {
     height: 100vh;
-    background-image: url('../../assets/images/background.png');
-    background-size: cover;
-    background-repeat: no-repeat;
+    margin-left: 8%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
-    .intro-text {
-      height: 100vh;
-      margin-left: 8%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
+    h1 {
+      font-family: "Russo One", sans-serif;
+      font-size: 160px;
 
-      h1 {
-        font-family: 'Russo One', sans-serif;
-        font-size: 160px;
+      @media screen and (max-width: 768px) {
+        font-size: 20vw;
+      }
+    }
+
+    h2 {
+      font-family: "Titillium Web", sans-serif;
+      font-size: 40px;
+      width: 70%;
+      padding: 25px 40px;
+      background-color: $aqua;
+
+      @media screen and (max-width: 768px) {
+        font-size: 6vw;
+        padding: 15px 25px;
+      }
+    }
+
+    button {
+      width: fit-content;
+      margin-top: 30px;
+      background-color: $orange;
+      border: none;
+      padding: 20px 45px;
+      font-size: 20px;
+      font-family: "Titillium Web", sans-serif;
+      text-transform: uppercase;
+      &:hover {
+        background-color: $dark;
       }
 
-      h2 {
-        font-family: 'Titillium Web', sans-serif;
-        font-size: 40px;
-        width: 70%;
-        padding: 25px 40px;
-        background-color: $aqua;
+      a {
+        text-decoration: none;
       }
 
-      button {
-        width: fit-content;
-        margin-top: 30px;
-        background-color: $orange;
-        border: none;
-        padding: 20px 45px;
-        font-size: 20px;
-        font-family: 'Titillium Web', sans-serif;
-        text-transform: uppercase;
-        &:hover {
-          background-color: $midnight;
-        }
-
-        a {
-          text-decoration: none;
-        }
+      @media screen and (max-width: 768px) {
+        font-size: 4vw;
+        padding: 15px 25px;
       }
     }
   }
-
+}
 </style>
