593aded2d3bbc320baf813a3f5d37465bf5e583c
NicoleAMS <nicole.sentis@gmail.com>
2020-12-12 16:50:03 +0100

    styles footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
-<div></div>
-  <!-- <h2>Footer Component</h2> -->
+<div class="footer">
+  <p>Designed & built by <a href="https://www.linkedin.com/in/nicolesentis/">Nicole Sentis</a></p>
+</div>
 </template>
 
 <script>
@@ -10,5 +11,29 @@ export default {
 </script>
 
 <style lang="scss">
+$dark: #081626;
+$white: #f1f7f3;
+$aqua: #3c95a8;
+
+.footer {
+  height: 50px;
+  width: 100%;
+  background-color: $white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  p {
+    color: $dark;
+    text-align: right;
+    margin-right: 15px;
+    font-size: 14px;
+
+    a {
+      color: $aqua;
+      text-decoration: none;
+    }
+  }
+}
 
 </style>
